4f5245773b70fcbb87f95669677b652c3835aced
Seongju Mun <seongju.moon@linux.com>
2025-05-05 14:55:32 +0900

    fix script.

--- Bkv2_sub_colosseum-agg/src/main/kotlin/book/k8sinfra/aggregateservice/controller/fix_agg_app_code.sh
+++ Bkv2_sub_colosseum-agg/src/main/kotlin/book/k8sinfra/aggregateservice/controller/fix_agg_app_code.sh
@@ -6,5 +6,4 @@ sed -i -e '/\/\/ culprit code > delete, logger.info > modify from abnormal to no
     }' \
     -e 's#^\([[:space:]]*\)//[[:space:]]*\(logger\.info("Detected normal response duration")\)#\1\2#' UserScoreController.kt
 git diff 
-echo "From build to run, the code is fixed"
-cd ../../../../../../..
+echo "Now go to build to ~/_Book_k8sInfra/ch6/6.6.3/Bkv2_sub_colosseum/"
