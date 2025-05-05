ff68c1e2b4829deab61a5c3096ca586fe3e74125
Seongju Mun <seongju.moon@linux.com>
2025-05-05 13:55:25 +0900

    Add stepover logic.

--- Bkv2_sub_colosseum-agg/src/main/kotlin/book/k8sinfra/aggregateservice/controller/fix_agg_app_code.sh
+++ Bkv2_sub_colosseum-agg/src/main/kotlin/book/k8sinfra/aggregateservice/controller/fix_agg_app_code.sh
@@ -5,3 +5,6 @@ sed -i -e '/\/\/ culprit code > delete, logger.info > modify from abnormal to no
         n;N;N;d
     }' \
     -e 's#^\([[:space:]]*\)//[[:space:]]*\(logger\.info("Detected normal response duration")\)#\1\2#' UserScoreController.kt
+git diff 
+echo "From build to run, the code is fixed"
+cd ../../../../../../..
