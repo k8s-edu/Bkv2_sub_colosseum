0029d2ca43ac775b835a382964f01332b4929af8
Seongju Mun <seongju.moon@linux.com>
2025-05-05 16:09:15 +0900

    rearrage path directory.

--- Bkv2_sub_colosseum-agg/fix_agg_app_code.sh
+++ Bkv2_sub_colosseum-agg/fix_agg_app_code.sh
@@ -4,6 +4,6 @@ echo "Fixing the code in UserScoreController.kt"
 sed -i -e '/\/\/ culprit code > delete, logger.info > modify from abnormal to normal/{
         N;N;N;d
     }' \
-    -e 's#^\([[:space:]]*\)//[[:space:]]*\(logger\.info("Detected normal response duration")\)#\1\2#' UserScoreController.kt
+    -e 's#^\([[:space:]]*\)//[[:space:]]*\(logger\.info("Detected normal response duration")\)#\1\2#' src/main/kotlin/book/k8sinfra/aggregateservice/controller/UserScoreController.kt
 git diff 
 echo "Now go to build to ~/_Book_k8sInfra/ch6/6.6.3/Bkv2_sub_colosseum/"
